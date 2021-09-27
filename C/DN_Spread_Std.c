cee967ba14bfaf131364e11604c40fb2808239f4
Trent Henderson <trent.henderson1@outlook.com>
2021-09-27 18:39:36 +1000

    Started catch24

---
+++ C/DN_Spread_Std.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "stats.h"
+
+double stddev(const double a[], const int size)
+{
+    double m = mean(a, size);
+    double sd = 0.0;
+    for (int i = 0; i < size; i++) {
+        sd += pow(a[i] - m, 2);
+    }
+    sd = sqrt(sd / (size - 1));
+    return sd;
+}
---
+++ C/DN_Spread_Std.h
@@ -0,0 +1,12 @@
+//
+//  Created by Trent Henderson 27 September 2021
+//
+
+#ifndef DN_SPREADSTD
+#define DN_SPREADSTD
+
+#include <stdio.h>
+
+extern double DN_Spread_Std(const double a[], const int size);
+
+#endif /* DN_SPREADSTD */
