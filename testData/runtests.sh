01f4de7ff08fd6b509f4b0b6ee8e530ef681e6c0
Brendan Harris <brendanjohnharris@gmail.com>
2022-06-13 01:15:47 +1000

    Update runtests.sh

--- testData/runtests.sh
+++ testData/runtests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../C/runAllTS.sh -i "$(dirname -- $0)" -o "$(dirname -- $0)" -a "_output" -s 1
+"$(dirname -- $0)/../C/runAllTS.sh" -i "$(dirname -- $0)" -o "$(dirname -- $0)" -a "_output" -s 1 -i "$(dirname -- $0)" -o "$(dirname -- $0)" -a "_output" -s 1
