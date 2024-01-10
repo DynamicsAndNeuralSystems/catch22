fce98378e9855d2ce1bbc29a53d7a27833cf74b0
Alberto Azzari <alberto.azzari@univr.it>
2024-01-10 10:21:32 +0100

    removed compiled files

--- fftw-3.3.10/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/albertoazzari/projects/catch22/fftw-3.3.10
-# Build directory: /Users/albertoazzari/projects/catch22/fftw-3.3.10/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
